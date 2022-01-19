7a24657598ef3d3ba25f5a73e9fc5f647b09de76
金戈宁 <626723063@qq.com>
2022-01-19 22:50:30 +0800

    fix: 拖动事件

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { resource, RESOURCE_TYPE, Game, GameObject } from '@eva/eva.js'
 import { RendererSystem } from '@eva/plugin-renderer'
 import { Img, ImgSystem } from '@eva/plugin-renderer-img' // 引入渲染图片所需要的组件和系统
+import { Event, EventSystem, HIT_AREA_TYPE } from '@eva/plugin-renderer-event'
 
 import './index.css'
 
@@ -36,23 +37,26 @@ export default function Home() {
   let gameObject2
 
   useEffect(() => {
-    // 创建渲染系统
-    const rendererSystem = new RendererSystem({
-      canvas: document.querySelector('#canvas'), // 可选，自动生成 canvas 挂在 game.canvas 上
-      width: 390,
-      height: 844,
-      transparent: false,
-      resolution: window.devicePixelRatio / 2, // 可选, 如果是2倍图设计 可以除以2
-      enableScroll: true, // 允许页面滚动
-      renderType: 0, // 0:自动判断，1: WebGL，2:Canvas，建议android6.1 ios9 以下使用Canvas，需业务判断。
-    })
-
-    // 创建游戏对象
     const game = new Game({
       frameRate: 60, // 可选，游戏帧率，默认60
       autoStart: true, // 可选，自动开始
-      systems: [rendererSystem],
+      transparent: false,
+      resolution: window.devicePixelRatio, // 可选, 如果是2倍图设计 可以除以2
+      enableScroll: true, // 允许页面滚动
+      renderType: 0, // 0:自动判断，1: WebGL，2:Canvas，建议android6.1 ios9 以下使用Canvas，需业务判断。z
+      systems: [
+        new RendererSystem({
+          canvas: document.querySelector('#canvas'),
+          width: 390,
+          height: 844,
+        }),
+        new EventSystem({
+          // moveWhenInside: true // 代表只有在元素内部才会执行move事件，默认为false
+        }),
+        new ImgSystem(),
+      ],
     })
+
     game.addSystem(new ImgSystem()) // 给游戏添加渲染图片的能力
 
     gameObject = new GameObject('gameObj1', {
@@ -78,6 +82,40 @@ export default function Home() {
         resource: 'image2',
       }),
     )
+    const evt = gameObject2.addComponent(
+      new Event({
+        // 使用这个属性设置交互事件可以触发的区域，骨骼动画有所变差，可以临时在当前游戏对象下添加一个同类型同属性的Graphic查看具体点击位置。
+        hitArea: {
+          type: HIT_AREA_TYPE.Polygon,
+          style: {
+            paths: [0, 0, 0, 150, 150, 150, 150, 0],
+          },
+        },
+      }),
+    )
+
+    let touched = false
+    evt.on('touchstart', e => {
+      console.log(e)
+      console.log('touchstart')
+      touched = true
+    })
+    evt.on('touchend', e => {
+      console.log('touchend')
+      touched = false
+    })
+    evt.on('touchmove', e => {
+      if (touched) {
+        const { gameObject, data } = e
+        const { transform } = gameObject
+        console.log('touchmove')
+        console.log('e', e)
+        console.log('gameObject', gameObject)
+        console.log('data', data)
+
+        transform.position = data.position
+      }
+    })
 
     game.scene.addChild(gameObject) // 把游戏对象放入场景，这样画布上就可以显示这张图片了
     game.scene.addChild(gameObject2) // 把游戏对象放入场景，这样画布上就可以显示这张图片了

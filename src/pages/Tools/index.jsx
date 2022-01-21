40057c837c98b1c55ca0d9990bee47536348fe28
金戈宁 <626723063@qq.com>
2022-01-22 02:23:39 +0800

    fix: 复制代码

--- src/pages/Tools/index.jsx
+++ src/pages/Tools/index.jsx
@@ -1,77 +1,158 @@
 import React, { useState, useEffect } from 'react'
 
-import { Game, GameObject, Component } from '@eva/eva.js'
-import { RendererSystem } from '@eva/plugin-renderer'
+import { GameObject, Component } from '@eva/eva.js'
 import { Img, ImgSystem } from '@eva/plugin-renderer-img' // 引入渲染图片所需要的组件和系统
-import { Physics, PhysicsSystem, PhysicsType } from '@eva/plugin-matterjs'
-import { Text, TextSystem } from '@eva/plugin-renderer-text'
-import { Graphics, GraphicsSystem } from '@eva/plugin-renderer-graphics'
-import { Event, EventSystem, HIT_AREA_TYPE } from '@eva/plugin-renderer-event'
+import { Physics, PhysicsType } from '@eva/plugin-matterjs'
 
+import { seaBgObj, shipObj, shipLightObj, footH } from '../../constant/objSettings'
 import gameInfo from '../../constant/game'
 
 import './index.css'
 
 export default function Tools() {
-  let gameObject
-  let gameObject2
-  let gameObject3
+  let seaBg
+  let ship
+  let shipLight
 
   useEffect(() => {
     const { game } = gameInfo
-    const { physics } = createPlayer()
-    const { evt } = createButton()
-    const walls = [createWall(0, 0, 20, 1000), createWall(750 - 20, 0, 20, 1000), createWall(0, 1000 - 20, 750, 20)]
 
-    physics.on('collisionStart', () => {
-      DrawRed()
+    game.addSystem(new ImgSystem()) // 给游戏添加渲染图片的能力
+    seaBg = new GameObject('seaBg', {
+      size: {
+        width: seaBgObj.w,
+        height: seaBgObj.h,
+      },
     })
-    physics.on('collisionEnd', () => {
-      DrawGreen()
+    ship = new GameObject('ship', {
+      size: {
+        width: shipObj.w,
+        height: shipObj.h,
+      },
+      position: {
+        x: +((seaBgObj.w - shipObj.w) / 2).toFixed(0),
+        y: seaBgObj.h - shipObj.h - footH,
+      },
     })
 
-    evt.on('tap', () => {
-      physics.body.force.y = -10
+    shipLight = new GameObject('shipLight', {
+      size: {
+        width: shipLightObj.w,
+        height: shipLightObj.h,
+      },
+      position: {
+        x: shipObj.oW - (shipLightObj.w / 2).toFixed(0),
+        y: shipObj.oH - (shipLightObj.h / 2).toFixed(0),
+      },
     })
+    seaBg.addComponent(
+      new Img({
+        resource: 'seaBg',
+      }),
+    )
+    ship.addComponent(
+      new Img({
+        resource: 'ship',
+      }),
+    )
 
-    function DrawRed() {
-      walls.forEach(wall => {
-        wall.drawColor(0xff0000)
-      })
-    }
-    function DrawGreen() {
-      walls.forEach(wall => {
-        wall.drawColor(0x00ffff)
-      })
-    }
+    ship.addComponent(
+      new Physics({
+        type: PhysicsType.RECTANGLE,
+        bodyOptions: {
+          isStatic: true,
+          // restitution: 0,
+          frictionAir: 0,
+          friction: 0.06,
+          frictionStatic: 0.3,
+          force: {
+            x: 0,
+            y: 0,
+          },
+        },
+        stopRotation: true,
+      }),
+    )
+    shipLight.addComponent(
+      new Img({
+        resource: 'shipLight',
+      }),
+    )
+    // const evt = ship.addComponent(
+    //   new Event({
+    //     // 使用这个属性设置交互事件可以触发的区域，骨骼动画有所变差，可以临时在当前游戏对象下添加一个同类型同属性的Graphic查看具体点击位置。
+    //     hitArea: {
+    //       type: HIT_AREA_TYPE.Polygon,
+    //       style: {
+    //         paths: [0, 0, 0, 150, 150, 150, 150, 0],
+    //       },
+    //     },
+    //   }),
+    // )
 
-    function createPlayer() {
-      const image = new GameObject('image', {
-        size: { width: 240, height: 240 },
-        origin: { x: 0.5, y: 0.5 },
+    // let touched = false
+    // let prePosition = { x: 0, y: 0 }
+    // evt.on('touchstart', e => {
+    //   // console.log(e)
+    //   // console.log('touchstart')
+    //   prePosition = e.data.position
+    //   touched = true
+    // })
+    // evt.on('touchend', e => {
+    //   // console.log('touchend')
+    //   touched = false
+    // })
+    // evt.on('touchmove', e => {
+    //   if (touched) {
+    //     const { gameObject, data } = e
+    //     const { transform } = gameObject
+
+    //     const position = {
+    //       x: transform.position.x + data.position.x - prePosition.x,
+    //       y: transform.position.y + data.position.y - prePosition.y,
+    //     }
+    //     prePosition = e.data.position
+
+    //     transform.position = position
+    //   }
+    //   document.addEventListener('visibilitychange', () => {
+    //     if (document.hidden) {
+    //       game.pause()
+    //     } else {
+    //       game.resume()
+    //     }
+    //   })
+    // })
+
+    game.scene.addChild(seaBg) // 把游戏对象放入场景，这样画布上就可以显示这张图片了
+    game.scene.addChild(shipLight)
+    game.scene.addChild(ship)
+
+    const blockImageNames = ['reef', 'fish1', 'shark']
+    setInterval(() => {
+      // console.log('开始添加')
+      const block = new GameObject('block', {
+        size: {
+          width: 80,
+          height: 80,
+        },
         position: {
-          x: 375,
-          y: 100,
-        },
-        scale: {
-          x: -1,
-          y: 1,
+          x: Math.floor(Math.random() * 390),
+          y: 0,
         },
       })
-
-      image.addComponent(
+      block.addComponent(
         new Img({
-          resource: 'imageName',
+          resource: blockImageNames[Math.floor(Math.random() * 3)],
         }),
       )
-
-      const physics = image.addComponent(
+      const physics = block.addComponent(
         new Physics({
           type: PhysicsType.RECTANGLE,
           bodyOptions: {
             isStatic: false,
             // restitution: 0,
-            frictionAir: 0.1,
+            frictionAir: 0.5 + (Math.random() * 4) / 10,
             friction: 0.06,
             frictionStatic: 0.3,
             force: {
@@ -79,126 +160,29 @@ export default function Tools() {
               y: 0,
             },
           },
-          stopRotation: true,
+          stopRotation: false,
         }),
       )
-      game.scene.addChild(image)
 
-      return { physics }
-    }
-
-    function createWall(x, y, width, height) {
-      const go = new GameObject('graphics', {
-        position: { x: x + width / 2, y: y + height / 2 },
-        size: { width, height },
-        origin: { x: 0.5, y: 0.5 },
+      physics.on('collisionStart', () => {
+        console.log('啊，撞到了')
       })
-      const graphics = go.addComponent(new Graphics())
-      graphics.graphics.beginFill(0x00ff00)
-      graphics.graphics.drawRect(0, 0, width, height)
-
-      go.addComponent(
-        new Physics({
-          type: PhysicsType.RECTANGLE,
-          bodyOptions: {
-            isStatic: true, // Whether the object is still, any force acting on the object in a static state will not produce any effect
-            restitution: 0.1,
-            frictionAir: 0,
-            friction: 0,
-            frictionStatic: 0,
-            force: {
-              x: 0,
-              y: 0,
-            },
-          },
-          stopRotation: true, // default false, usually do not need to be set
-        }),
-      )
-      game.scene.addChild(go)
-
-      return {
-        drawColor(color) {
-          graphics.graphics.beginFill(color)
-          graphics.graphics.drawRect(0, 0, width, height)
-        },
-      }
-    }
-
-    function createButton() {
-      const textGO = new GameObject('text', {
-        origin: {
-          x: 0.5,
-          y: 0.5,
-        },
-        anchor: {
-          x: 0.5,
-          y: 0.5,
-        },
-      })
-      textGO.addComponent(
-        new Text({
-          text: 'Jump',
-          style: {
-            fill: 0xffffff,
-            fontSize: 50,
-          },
-        }),
-      )
-      const go = new GameObject('button', {
-        position: {
-          x: 750 - 30,
-          y: 1334 - 30,
-        },
-        origin: {
-          x: 1,
-          y: 1,
-        },
-      })
-      const { graphics } = go.addComponent(new Graphics())
-      graphics.beginFill(0x00ffff)
-      const w = 240
-      const h = 120
-      graphics.drawRoundedRect(0, 0, w, h)
-      go.transform.size.width = w
-      go.transform.size.height = h
-      go.addChild(textGO)
-      game.scene.addChild(go)
-
-      const evt = go.addComponent(new Event())
-      return { evt }
-    }
+      game.scene.addChild(block)
+    }, 1500)
   }, [])
 
   return (
     <>
-      <h1
-        onClick={() => {
-          gameObject2.transform.position.y = gameObject2.components[0].position.y - 10
-        }}
-      >
-        上
-      </h1>
-      <h1
-        onClick={() => {
-          gameObject2.transform.position.y = gameObject2.components[0].position.y + 10
-        }}
-      >
-        下
-      </h1>
-      <h1
-        onClick={() => {
-          gameObject2.transform.position.x = gameObject2.components[0].position.x - 10
-        }}
-      >
-        左
-      </h1>
-      <h1
-        onClick={() => {
-          gameObject2.transform.position.x = gameObject2.components[0].position.x + 10
-        }}
-      >
-        右
-      </h1>
+      <div className="home_bg">
+        <div
+          className="home_top"
+          onClick={() => {
+            console.log('xxx')
+          }}
+        >
+          111
+        </div>
+      </div>
     </>
   )
 }

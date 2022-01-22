72886eb388bcb3beb9d47fbb57eb26e9dcca3ed3
金戈宁 <626723063@qq.com>
2022-01-22 13:21:03 +0800

    fix: 附带层级

--- src/pages/Tools/index.jsx
+++ src/pages/Tools/index.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { GameObject, Component } from '@eva/eva.js'
 import { Img, ImgSystem } from '@eva/plugin-renderer-img' // 引入渲染图片所需要的组件和系统
 import { Physics, PhysicsType } from '@eva/plugin-matterjs'
+import { Render } from '@eva/plugin-renderer-render'
 
 import { seaBgObj, shipObj, shipLightObj, footH } from '../../constant/objSettings'
 import gameInfo from '../../constant/game'
@@ -16,10 +17,70 @@ export default function Tools() {
   let physics
   let touchX
 
-  useEffect(() => {
-    const { game } = gameInfo
+  /**
+   * 制造波浪
+   */
+  const createOceanCurrentPattern = ({
+    width = 360,
+    height = 720,
+    x = -100,
+    y = 800,
+    speed = 200,
+    speedC = 25,
+    rotation = 0,
+  }) => {
+    const { container } = gameInfo
 
-    game.addSystem(new ImgSystem()) // 给游戏添加渲染图片的能力
+    const block = new GameObject('oceanCurrent', {
+      origin: {
+        x: 0.5,
+        y: 0.5,
+      },
+      size: {
+        width,
+        height,
+      },
+      position: {
+        x,
+        y,
+      },
+      rotation,
+    })
+    block.addComponent(
+      new Img({
+        resource: 'oceanCurrentPattern',
+      }),
+    )
+    block.addComponent(
+      new Render({
+        zIndex: 2,
+      }),
+    )
+    // 存在10秒后自动清除
+    setTimeout(() => {
+      container.removeChild(block)
+    }, 15000)
+    const move = speed => {
+      block.transform.position.x += speed / speedC
+      if (speed === 0) {
+        container.removeChild(block)
+        return
+      }
+      requestAnimationFrame(() => {
+        move(speed > 0 ? speed - 1 : speed + 1)
+      })
+    }
+    container.addChild(block)
+    requestAnimationFrame(() => {
+      move(speed)
+    })
+  }
+
+  useEffect(() => {
+    const { game, container } = gameInfo
+
+    game.scene.addChild(container)
+
     seaBg = new GameObject('seaBg', {
       size: {
         width: seaBgObj.w,
@@ -57,6 +118,11 @@ export default function Tools() {
         resource: 'ship',
       }),
     )
+    ship.addComponent(
+      new Render({
+        zIndex: 3,
+      }),
+    )
 
     ship.addComponent(
       new Physics({
@@ -80,10 +146,15 @@ export default function Tools() {
         resource: 'shipLight',
       }),
     )
+    shipLight.addComponent(
+      new Render({
+        zIndex: 2,
+      }),
+    )
 
-    game.scene.addChild(seaBg) // 把游戏对象放入场景，这样画布上就可以显示这张图片了
-    game.scene.addChild(shipLight)
-    game.scene.addChild(ship)
+    container.addChild(seaBg) // 把游戏对象放入场景，这样画布上就可以显示这张图片了
+    container.addChild(shipLight)
+    container.addChild(ship)
 
     const blockImageNames = ['reef', 'fish1', 'shark']
     const genBlock = () => {
@@ -101,6 +172,11 @@ export default function Tools() {
       block.addComponent(
         new Img({
           resource: blockImageNames[Math.floor(Math.random() * 3)],
+        }),
+      )
+      block.addComponent(
+        new Render({
+          zIndex: 3,
         }),
       )
       physics = block.addComponent(
@@ -124,11 +200,11 @@ export default function Tools() {
       physics.on('collisionStart', () => {
         // console.log('啊，撞到了')
       })
-      game.scene.addChild(block)
+      container.addChild(block)
 
       // 存在10秒后自动清除
       setTimeout(() => {
-        game.scene.removeChild(block)
+        container.removeChild(block)
       }, 10000)
       setTimeout(() => {
         requestAnimationFrame(genBlock)
@@ -147,16 +223,16 @@ export default function Tools() {
     const { components } = phyObj
 
     // 视图位置
-    components[2].body.position[pos] -= num
+    components[3].body.position[pos] -= num
     // 物理位置
     // 四边形
-    components[2].body.bounds.max[pos] -= num
-    components[2].body.bounds.min[pos] -= num
+    components[3].body.bounds.max[pos] -= num
+    components[3].body.bounds.min[pos] -= num
     // 多边形 待验证
-    components[2].body.vertices[0][pos] -= num
-    components[2].body.vertices[1][pos] -= num
-    components[2].body.vertices[2][pos] -= num
-    components[2].body.vertices[3][pos] -= num
+    components[3].body.vertices[0][pos] -= num
+    components[3].body.vertices[1][pos] -= num
+    components[3].body.vertices[2][pos] -= num
+    components[3].body.vertices[3][pos] -= num
   }
   /**
    * 移动单位
@@ -185,11 +261,18 @@ export default function Tools() {
           const moveX = e.changedTouches[0].clientX - touchX
           if (moveX < -500) {
             console.log('向左')
+            createOceanCurrentPattern({
+              x: 750,
+              y: 800,
+              speed: -200,
+              rotation: 3.15,
+            })
             movePhy(ship, 50, 'x')
             moveObj(shipLight, 50, 'x')
           }
           if (moveX > 500) {
             console.log('向右')
+            createOceanCurrentPattern({})
             movePhy(ship, -50, 'x')
             moveObj(shipLight, -50, 'x')
           }
